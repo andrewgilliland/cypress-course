c0e4cfcfba8461fc9adcade5a86bc83261b8cecc
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-05-08 13:27:34 -0500

    initial commit

---
+++ cypress/e2e/fundamentals.cy.js
@@ -0,0 +1,9 @@
+describe("Fundamentals test", () => {
+  it("Contains correct header text", () => {
+    cy.visit("/fundamentals");
+
+    cy.get("[data-test='fundamentals-header']").contains(
+      /Testing Fundamentals/i
+    );
+  });
+});
